5d3e1decde654e88922004f4c56e9fa2dd42bf53
Fabian Canas <fabian@mojotech.com>
2014-07-29 09:53:16 -0400

    Initial Commit

---
+++ DropPin/MOJOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MOJOAppDelegate.h
+//  DropPin
+//
+//  Created by Fabian Canas on 7/29/14.
+//  Copyright (c) 2014 MojoTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MOJOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DropPin/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DropPin
+//
+//  Created by Fabian Canas on 7/29/14.
+//  Copyright (c) 2014 MojoTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MOJOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MOJOAppDelegate class]));
+    }
+}
